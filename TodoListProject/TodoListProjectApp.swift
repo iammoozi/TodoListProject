55442b88abea85d5cc1ce54f7b5a387b8e0f8b24
MooziKim <jaeyoon7421@naver.com>
2024-10-17 15:11:30 +0900

    Initial Commit

---
+++ TodoListProject/TodoListProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListProjectApp.swift
+//  TodoListProject
+//
+//  Created by 김재윤 on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
